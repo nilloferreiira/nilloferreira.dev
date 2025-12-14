a4490571414f4a51a969364f2bdd70e7a149cb29
Danillo Ferreira <nilloferreiira@gmail.com>
2025-12-14 13:02:26 -0300

    feat: remove varibles from env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,9 +1,7 @@
 import { z } from "zod"
 
 const EnvSchema = z.object({
-	DATABASE_URL: z.url(),
-	ADMIN_EMAIL: z.email(),
-	ADMIN_PASSWORD: z.string().min(6)
+	DATABASE_URL: z.url()
 })
 
 export const env = EnvSchema.parse(process.env)
